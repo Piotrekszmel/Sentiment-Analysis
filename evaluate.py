52769018a0b9903bce71f1d269a90cd63c93cee1
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-09-12 23:30:58 +0200

    removed hist.pickle and evaluate.py

--- evaluate.py
+++
@@ -1,7 +0,0 @@
-import pickle
-
-word_indices = pickle.load(open("model_word_indices.pickle", "rb"))
-
-first2pairs = {k: word_indices[k] for k in sorted(word_indices.keys())[:2]}
-print(first2pairs)
-
