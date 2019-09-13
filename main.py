005b5ef09f7fc542f9dc33933f8d036154fb5836
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-09-13 16:26:49 +0200

    deleted unnecessary imports

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-from models.baseline_nbow import baseline_nbow
-from models.sentiment_model import Sentiment_Analysis
-import pickle
 import os
-import numpy as np
-import sys
-from keras.callbacks import ModelCheckpoint
-from keras.layers import LSTM
-from utilities.callbacks import MetricsCallback, PlottingCallback
-from utilities.data_preparation import get_labels_to_categories_map, get_class_weights2, onehot_to_categories
-from sklearn.metrics import f1_score, precision_score
-from sklearn.metrics import recall_score
-from data.data_loader import DataLoader
-from models.nn_models import build_attention_RNN
-from utilities.data_loader import get_embeddings, Loader, prepare_dataset
 from evaluate import predict_class
 
 
